e1f41625128905cfb55ab3f1d47fb443b8d0d166
Harsh Chauhan <harshchauhan41133@gmail.com>
2025-08-26 14:29:11 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(
+    name = "greenvision",
+    author="harsh",
+    author_email="harshc8179@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
